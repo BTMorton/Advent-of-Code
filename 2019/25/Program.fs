384f3d4d9a4ce81249afc8620c8272c43c7bee3b
Ben Morton <ben.morton91@gmail.com>
2020-01-03 04:49:50 +0000

    Add 2019 day 25

---
+++ 2019/25/Program.fs
@@ -0,0 +1,43 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open IntCode.IntCode
+
+let printOutput output =
+    printfn "%s"
+        (output
+        |> List.rev
+        |> List.map char
+        |> (fun l -> String.Join("", l)))
+
+let processInput line =
+    line
+    |> Seq.map int64
+    |> Seq.toList
+
+let runProgram computer =
+    let rec iter computer =
+        let computer = resumeComputer computer
+        printOutput computer.output
+
+        let line = Console.ReadLine ()
+
+        if line = "exit"
+        then ()
+        else
+            let computer = {
+                computer with
+                    input = processInput (line + "\n")
+                    output = []
+            }
+            iter computer
+
+    iter computer
+
+[<EntryPoint>]
+let main argv =
+    let opts = loadFile "data.txt"
+
+    let computer = createComputer opts []
+    runProgram computer
+    0 // return an integer exit code
