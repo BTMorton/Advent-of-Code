f2e63b40832824ea7a05501a60eabf3275421412
Ben Morton <ben.morton91@gmail.com>
2020-01-03 07:54:13 +0000

    Add 2015 day 25

---
+++ 2015/25/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let getNextCode code =
+    (code * 252533L) % 33554393L
+
+let getEntryNumber x y =
+    let rec sum n =
+        match n with
+        | 0 -> 0
+        | n -> sum (n - 1) + n
+
+    (sum (x + y - 2)) + (x - 1)
+
+[<EntryPoint>]
+let main argv =
+    let entry = getEntryNumber 3019 3010
+    let code = List.replicate entry getNextCode
+                  |> List.fold (fun code fn -> fn code) 20151125L
+
+    printfn "Part 1: %d" (code)
+    0 // return an integer exit code
